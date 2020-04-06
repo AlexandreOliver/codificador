52f6f08d49637a74da2fdff313996b7f0e8cab45
Richard Cauê Tavares Smanhoto <richard.11smanhoto@gmail.com>
2020-04-06 12:48:26 -0300

    Simplificação na função

--- complement/Functions.py
+++ complement/Functions.py
@@ -7,7 +7,6 @@ def codific():
     for ind in listapalavra:  # para cada índice na lista:
         if ind in codificar.keys():  # se esse indice estiver nas chaves do dict codificar
             newpalavra.append(codificar[ind])  # adiciona o valor dessa chave em uma nova lista
-        elif ind in " ":  # se tiver um espaço
-            newpalavra.append(ind)  # adiciona
-        elif ind in ",.:;!?@#$%&*(){}[]<>_-+=/°":
+        elif ind in " ,.:;!?@#$%&*(){}[]<>_-+=/°":
             newpalavra.append(ind)  # add
+
